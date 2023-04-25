78f6a8b15e69a6e0bb4ed29900b7b2eebf8c6c3c
hmills21 <hailemillss@gmail.com>
2023-04-25 10:44:10 -0700

    sayHello function

--- conditionals.js
+++ conditionals.js
@@ -20,13 +20,11 @@ if (temperature < 0) {
 } */
 
 const whichSchool = function (age) {
- if (age < 13) {
-  console.log("Elementary School");
-} else if (age >= 13 && age <= 18) {
-  console.log("Secondary school");
-} else {
-  console.log("Lighthouse Labs");
-} 
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return "Lighthouse Labs";
+  } 
 }
-
-whichSchool(18);
